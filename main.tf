4726f32eaf5dae216032b6ae38c02fea586e64ac
Przemo <przemo06133@gmail.com>
2023-10-31 09:29:28 +0100

    Add vpc with ref to subnet

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
 provider "aws" {
   region = "eu-central-1"
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name: "dev"
+  }
 }
 
-resource "aws_instance" "my-server" {
-  ami = "ami-06dd92ecc74fdfb36"
-  instance_type = "t2.micro"
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "dev-subnet"
+  }
 }
