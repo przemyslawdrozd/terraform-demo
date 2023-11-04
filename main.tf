440701056e7495db89fa6493dacd6e8311e91bd4
Przemo <przemo06133@gmail.com>
2023-11-04 20:47:49 +0100

    Create infrastructure for web server

--- main.tf
+++ main.tf
@@ -1,17 +1,103 @@
 provider "aws" {
   region = "eu-central-1"
-resource "aws_vpc" "first-vpc" {
+}
+resource "aws_vpc" "my-vpc" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    name: "dev"
+}
+
+resource "aws_internet_gateway" "my-gw" {
+  vpc_id = aws_vpc.my-vpc.id
+}
+
+resource "aws_route_table" "my-route-table" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-gw.id
+  }
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.my-gw.id
+  }
+}
+resource "aws_subnet" "my-subnet" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+}
+resource "aws_route_table_association" "my-rt-asso" {
+  subnet_id      = aws_subnet.my-subnet.id
+  route_table_id = aws_route_table.my-route-table.id
+}
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description      = "HTTPS traffic"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "HTTP traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "SSH traffic"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.first-vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "dev-subnet"
-  }
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.my-subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
 }
+
+resource "aws_eip" "my-eip" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [ aws_internet_gateway.my-gw ]
+}
+
+# resource "aws_instance" "my-server" {
+#   ami               = "ami-06dd92ecc74fdfb36"
+#   instance_type     = "t2.micro"
+#   availability_zone = "eu-central-1a"
+#   key_name          = "main-key"
+
+#   network_interface {
+#     network_interface_id = aws_network_interface.web-server-nic.id
+#     device_index         = 0
+#   }
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               sudo apt update -y
+#               sudo apt install apache2 -y
+#               sudo systemctl start apache2
+#               sudo bash -c 'echo yout web server > /var/www/html/index.html
+#               EOF
+# }
